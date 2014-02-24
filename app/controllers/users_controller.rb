2227c47726ef7bf877a889ca948cae99355ea615
Dave Nearing <danearing@Serenity.(none)>
2014-02-24 11:15:01 -0600

    Resolved Issue #9 Bad Permissions Logic Gate.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -117,6 +117,20 @@ require'socket'
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
